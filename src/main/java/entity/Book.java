5d679d10e4afba0276ee40451fc1330afccd08b9
zlotfi <zlotfi758@gmail.com>
2023-09-30 12:57:06 +0330

    add needed attribute and constructor, getter and setter

--- src/main/java/entity/Book.java
+++ src/main/java/entity/Book.java
@@ -1,4 +1,48 @@
 package entity;
 
 public class Book {
+
+    private Integer bookId;
+    private String title;
+    private String printYear;
+    private String authorBook;
+
+    public Book(Integer bookId, String title, String printYear, String authorBook) {
+        this.bookId = bookId;
+        this.title = title;
+        this.printYear = printYear;
+        this.authorBook = authorBook;
+    }
+
+    public Integer getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Integer bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPrintYear() {
+        return printYear;
+    }
+
+    public void setPrintYear(String printYear) {
+        this.printYear = printYear;
+    }
+
+    public String getAuthorBook() {
+        return authorBook;
+    }
+
+    public void setAuthorBook(String authorBook) {
+        this.authorBook = authorBook;
+    }
 }
