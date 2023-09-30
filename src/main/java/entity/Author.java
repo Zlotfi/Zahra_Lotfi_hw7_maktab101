24c9dcac20521d4c6b116b2ae4d1f5c5b9916ca6
zlotfi <zlotfi758@gmail.com>
2023-09-30 12:50:44 +0330

    add needed attribute and constructor, getter and setter

--- src/main/java/entity/Author.java
+++ src/main/java/entity/Author.java
@@ -1,4 +1,57 @@
 package entity;
 
 public class Author {
+    private Integer authorId;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String[] bookList = new String[3];
+
+    public Author(Integer authorId, String firstName, String lastName, int age, String[] bookList) {
+        this.authorId = authorId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.bookList = bookList;
+    }
+
+    public Integer getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(Integer authorId) {
+        this.authorId = authorId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String[] getBookList() {
+        return bookList;
+    }
+
+    public void setBookList(String[] bookList) {
+        this.bookList = bookList;
+    }
 }
