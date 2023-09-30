c997a363366f3faf9fabdf21f887df030e11c857
zlotfi <zlotfi758@gmail.com>
2023-09-30 17:14:30 +0330

    add register method

--- src/main/java/service/AuthorService.java
+++ src/main/java/service/AuthorService.java
@@ -1,4 +1,23 @@
 package service;
 
+import entity.Author;
+import repository.AuthorRepository;
+
+import java.sql.SQLException;
+
 public class AuthorService {
+
+    private final AuthorRepository authorRepository = new AuthorRepository();
+
+    public AuthorService() throws SQLException {
+    }
+
+    public void register(String firstName,String lastName,int age) throws SQLException {
+        Author author = new Author(null,"parvin","etesami",80, new String[]{"divan"});
+        int result = authorRepository.save(author);
+        if(result != 0)
+            System.out.println(author.getFirstName() + " successfully added to database");
+        else
+            System.out.println("OOps! :(");
+    }
 }
