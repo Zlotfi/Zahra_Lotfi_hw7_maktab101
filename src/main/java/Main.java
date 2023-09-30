395bd1a2321284b953a8044c8c5d1a854d5b68b0
zlotfi <zlotfi758@gmail.com>
2023-09-30 15:20:22 +0330

    test for load method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,11 +2,17 @@ import entity.Author;
 import repository.AuthorRepository;
 
 import java.sql.SQLException;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
         AuthorRepository authorRepository = new AuthorRepository();
-        Author author = new Author(null,"jamal","mir sadeghi",90, new String[]{"rahaei"});
-        authorRepository.save(author);
+//        Author author = new Author(null,"jamal","mir sadeghi",90, new String[]{"rahaei"});
+//        authorRepository.save(author);
+        Author author = authorRepository.load(1);
+        System.out.println(author.getFirstName());
+        System.out.println(author.getLastName());
+        System.out.println(author.getAge());
+        System.out.println(Arrays.toString(author.getBookList()));
     }
 }
