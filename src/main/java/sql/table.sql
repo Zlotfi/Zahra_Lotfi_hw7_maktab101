186b616faf8e1f06d21aed1f88e1669e647a9b89
zlotfi <zlotfi758@gmail.com>
2023-09-30 14:13:38 +0330

    add command for create author table

---
+++ src/main/java/sql/table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS author(
+    authorId SERIAL PRIMARY KEY,
+    firstName VARCHAR(50) NOT NULL,
+    lastName VARCHAR(50) NOT NULL,
+    age INT NOT NULL,
+    bookList TEXT[] NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS book(
+
+)
