c73bb0f3c216faa6e02f86f46fc7cd6fe253111f
zlotfi <zlotfi758@gmail.com>
2023-09-30 15:42:37 +0330

    test for load method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,27 @@
 import entity.Author;
+import entity.Book;
 import repository.AuthorRepository;
+import repository.BookRepository;
 
 import java.sql.SQLException;
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        AuthorRepository authorRepository = new AuthorRepository();
+//        AuthorRepository authorRepository = new AuthorRepository();
 //        Author author = new Author(null,"jamal","mir sadeghi",90, new String[]{"rahaei"});
 //        authorRepository.save(author);
-        Author author = authorRepository.load(1);
-        System.out.println(author.getFirstName());
-        System.out.println(author.getLastName());
-        System.out.println(author.getAge());
-        System.out.println(Arrays.toString(author.getBookList()));
+//        Author author = authorRepository.load(1);
+//        System.out.println(author.getFirstName());
+//        System.out.println(author.getLastName());
+//        System.out.println(author.getAge());
+//        System.out.println(Arrays.toString(author.getBookList()));
+        BookRepository bookRepository = new BookRepository();
+//        Book book = new Book(null,"rahaei","1392","jamal mir sadeghi");
+//        bookRepository.save(book);
+        Book book = bookRepository.load(1);
+        System.out.println("book title: " + book.getTitle());
+        System.out.println("printYear book: " + book.getPrintYear());
+        System.out.println("Author book: " + book.getAuthorBook());
     }
 }
