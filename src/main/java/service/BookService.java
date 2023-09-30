a1470a293783f613f1bfde85bcc8489df7f080c4
zlotfi <zlotfi758@gmail.com>
2023-09-30 17:25:42 +0330

    add addBook method

--- src/main/java/service/BookService.java
+++ src/main/java/service/BookService.java
@@ -1,4 +1,24 @@
 package service;
 
+import entity.Author;
+import entity.Book;
+import repository.BookRepository;
+
+import java.sql.SQLException;
+
 public class BookService {
+
+    private final BookRepository bookRepository = new BookRepository();
+
+    public BookService() throws SQLException {
+    }
+
+    public void addBook() throws SQLException {
+        Book book = new Book(null,"an sooye marg","1394","jamal sadeghi");
+        int result = bookRepository.save(book);
+        if(result != 0)
+            System.out.println(book.getTitle() + " successfully added to database");
+        else
+            System.out.println("OOps! :(");
+    }
 }
