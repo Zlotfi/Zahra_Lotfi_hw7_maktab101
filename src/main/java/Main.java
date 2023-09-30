80ab781a218a9d6a8d58d19d0c371798edc4ce42
zlotfi <zlotfi758@gmail.com>
2023-09-30 14:53:13 +0330

    test save method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,12 @@
+import entity.Author;
+import repository.AuthorRepository;
+
+import java.sql.SQLException;
+
 public class Main {
+    public static void main(String[] args) throws SQLException {
+        AuthorRepository authorRepository = new AuthorRepository();
+        Author author = new Author(null,"jamal","mir sadeghi",90, new String[]{"rahaei"});
+        authorRepository.save(author);
+    }
 }
